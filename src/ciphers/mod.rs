c461a8ee0c492ca1b1e46cec438a6b264743a651
laineysg <laineygreen97@gmail.com>
2024-06-24 21:57:14 -0700

    Add vigenere cipher and update driver

--- src/ciphers/mod.rs
+++ src/ciphers/mod.rs
@@ -1,3 +1,5 @@
+use ascii::AsciiStr;
+const LOWERCASE_ASCII_OFFSET: i32 = 97;
 pub fn shift_char(c: char, shift: i32) -> char { 
     //shifts char by converting the character to an 8bit int, then to a 32bit int, adds the shift integer value
     //converts the new value back to an 8 bit int, and finally to a char. Returns the new, shifted char.
@@ -13,4 +15,29 @@ pub fn caesar_cipher(message: &str, shift: i32, enc_type: &str) -> String {
         result.push(shift_char(c, shift));
     }
     result //Then return result
+}
+
+pub fn vigenere_cipher(message: &str, key: &str, enc_type: &str) -> String {
+    let mut result = String::new();
+    let mut key_cursor:usize = 0;
+    for (_idx, current_char) in message.chars().enumerate() { //returns index and char for each char in message.
+        if key_cursor >= key.chars().count() { //If cursor is out of bounds (at the end of the key)
+            key_cursor = 0;    
+        }
+        let indexed_key = AsciiStr::from_ascii(key).unwrap();
+        let key_ascii_arr = indexed_key.as_slice();
+        
+
+        let mut shift = 0;
+        if enc_type.contains("enc") {
+            shift = (key_ascii_arr[key_cursor] as i32) - LOWERCASE_ASCII_OFFSET;
+            println!("shift enc: {}", shift);
+        } else if enc_type.contains("dec") {
+            shift = -((key_ascii_arr[key_cursor] as i32) - LOWERCASE_ASCII_OFFSET);
+            println!("shift dec: {}", shift);
+        }
+        key_cursor += 1;
+        result += &shift_char(current_char, shift).to_string();
+    }
+    result
 }
--- src/main.rs
+++ src/main.rs
@@ -12,52 +12,80 @@ fn main() {
         io::stdin().read_line(&mut user_choice).expect("Failed to read user input!");
 
         //check for input, if there 
-        let valid_ceasar_options = ["cea", "cae", "1"];
-        if valid_ceasar_options.iter().any(|&option| user_choice.trim().to_lowercase().contains(option)) { //if caesar is chosen
-            println!("A caesar cipher is a common substitution cipher that shifts letters by \
-            a key called the shift value. Is this what you would like to do?");
+        match user_choice.trim().to_lowercase() {
+            opt if opt.contains("cae") || opt.contains("cea") => {
+                println!("A caesar cipher is a common monoalphabetic substitution cipher that shifts letters by \
+                a key called the shift value. Is this what you would like to do?");
 
-            //read input
-            io::stdin().read_line(&mut user_choice).expect("Failed to read user input!");
+                //read input
+                io::stdin().read_line(&mut user_choice).expect("Failed to read user input!");
 
 
-            let valid_yes_options = ["y","1"];
-            if valid_yes_options.iter().any(|&option| user_choice.trim().to_lowercase().contains(option)) { 
-                println!("Please enter a comma separated list consisting of your message, the shift value, and whether you will be \
-                encrypting or decrypting the message. For example, \"secretmessage,8,enc\"");
- 
-                io::stdin().read_line(&mut user_vars).expect("Failed to read user input!");
-                let args: Vec<&str> = user_vars.split(',').collect();
-                let valid_type_options = ["enc","dec"];
-                if args[1].trim().to_lowercase().parse::<i32>().is_ok() && valid_type_options.iter().any(|&option| args[2].trim().to_lowercase().contains(option)) { 
-                    let shift_key = args[1].trim().to_lowercase().parse::<i32>();
-                    match shift_key {
-                        Ok(shift_key) => {
-                            let ciphertext = ciphers::caesar_cipher(args[0],shift_key,args[2]);
-                            println!("Your ciphertext is: \t {}",ciphertext);
-                        },
-                        Err(_) => {
-                            println!("Shift key must be an integer, please try again.")
+                let valid_yes_options = ["y","1"];
+                if valid_yes_options.iter().any(|&option| user_choice.trim().to_lowercase().contains(option)) { 
+                    println!("Please enter a comma separated list consisting of your message, the shift value, and whether you will be \
+                    encrypting or decrypting the message. For example, \"secretmessage,8,enc\"");
+    
+                    io::stdin().read_line(&mut user_vars).expect("Failed to read user input!");
+                    let args: Vec<&str> = user_vars.split(',').collect();
+                    let valid_type_options = ["enc","dec"];
+                    if args[1].trim().to_lowercase().parse::<i32>().is_ok() && valid_type_options.iter().any(|&option| args[2].trim().to_lowercase().contains(option)) { 
+                        let shift_key = args[1].trim().to_lowercase().parse::<i32>();
+                        match shift_key {
+                            Ok(shift_key) => {
+                                let ciphertext = ciphers::caesar_cipher(args[0],shift_key,args[2]);
+                                println!("Your ciphertext is: \t {}",ciphertext);
+                            },
+                            Err(_) => {
+                                println!("Shift key must be an integer, please try again.")
+                            }
                         }
+                    } else {
+                        println!("There was an error with this input!");
                     }
                 } else {
-                    println!("There was an error with this input!")
+                    println!("Please try selecting a cipher again.");
                 }
-            } else {
-                println!("Please try selecting a cipher again.");
             }
-        } else {
-            println!("Your response didn't seem to have any cipher name in it. Please try again.");
-        }
-        //Check exit condition
-        if user_choice.trim().to_lowercase().contains("exit") {
-            println!("Exiting program!");
-            break;
-        }
-        //Check help condition
-        if user_choice.trim().to_lowercase().contains("help") {
-            println!("Enter a valid cipher option. Valid options include the following: 
-            caesar cipher,");
+            opt if opt.contains("vig") => {
+                println!("A vigenere cipher is a common polyalphabetic substitution cipher that shifts letters by \
+                the values of a repeating key. Is this what you would like to do?");
+
+                //read input
+                io::stdin().read_line(&mut user_choice).expect("Failed to read user input!");
+
+
+                let valid_yes_options = ["y","1"];
+                if valid_yes_options.iter().any(|&option| user_choice.trim().to_lowercase().contains(option)) { 
+                    println!("Please enter a comma separated list consisting of your message, the string key, and whether you will be \
+                    encrypting or decrypting the message. For example, \"secretmessage,secretkey,enc\"");
+    
+                    io::stdin().read_line(&mut user_vars).expect("Failed to read user input!");
+                    let args: Vec<&str> = user_vars.split(',').collect();
+                    let valid_type_options = ["enc","dec"];
+                    if valid_type_options.iter().any(|&option| args[2].trim().to_lowercase().contains(option)) { 
+                            let ciphertext = ciphers::vigenere_cipher(args[0],args[1],args[2]);
+                            println!("Your ciphertext is: \t {}",ciphertext);
+                    } else {
+                        println!("There was an error with this input!");
+                    }
+                } else {
+                    println!("Please try selecting a cipher again.");
+                }
+            }
+            opt if opt.contains("help") => {
+                println!("Enter a valid cipher option. Valid options include the following: 
+                caesar cipher,
+                vigenere cipher,
+                Note: You don't need to enter the full name, you only have to enter enough of the name to register as uniquely one cipher (ie, cae and vig both will work)");
+            }
+            opt if opt.contains("exit") => {
+                println!("Exiting program!");
+                break;
+            }
+            _ => {
+                println!("No cipher was detected! Please try again.");
+            }
         }
     }
 }
